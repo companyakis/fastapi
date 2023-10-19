b8acc595f4ff61081b02ec63ac0866a437f46ac9
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-10-19 20:59:48 +0300

    Create 02-type-control1.py

---
+++ 02-type-control1.py
@@ -0,0 +1,18 @@
+# newer versions of python (. > Python 3.9)
+
+# int list
+
+ages: list[int] = [43, 33, 10, 7]
+
+# tuple, immutable
+# name, age, marital status, country
+
+her_info: tuple[str, int, bool, str] = ("gunay", 33, True, "canada")
+
+# item name and price dictionary
+
+item_info: dict[str, float] = {
+    "computer": 15250.4,
+    "table": 2000.0,
+}
+
